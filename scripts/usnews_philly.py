2b65466834281422ccd2ae692692d0dc112a82bb
Amanda Kreider <akreid@wharton.upenn.edu>
2023-01-04 16:08:55 -0500

    Update usnews_philly.py

--- scripts/usnews_philly.py
+++ scripts/usnews_philly.py
@@ -11,9 +11,6 @@ city = 'Philadelphia, PA'
 # Define url
 usn_url = 'https://health.usnews.com'
 
-# Define timezone
-eastern = pytz.timezone("America/New_York")
-
 # Define headers
 hdr = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'
